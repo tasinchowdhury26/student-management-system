6e8aa2176abcbfa31b97f8c517ee33ab4d8542b4
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2022-06-10 03:21:03 +0600

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("RehanaKabirMim");
+		
+		System.out.println("Student Management System");
 
 	}
 
